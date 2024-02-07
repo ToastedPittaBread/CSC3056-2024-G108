fc0cfa191c6ca74b7826f07cb2212c1cee966963
KBennett9 <159037166+KBennett9@users.noreply.github.com>
2024-02-07 09:18:47 +0000

    TODO5 Commit

--- Lab1/Mini_lab_Java_Refresher_1_Simple_banking_app/src/tests/UserTest.java
+++ Lab1/Mini_lab_Java_Refresher_1_Simple_banking_app/src/tests/UserTest.java
@@ -30,6 +30,16 @@ public class UserTest {
 		
 		// 3-Verify (Assert)
 		System.out.println("Starting the assertions of the test method: testUserConstructor");
+
+		assert (testUser.getUsername() == testUsername)
+		
+		assert (testUser.getPassword() == testPassword)
+		
+		assert (testUser.getFirstName() == testFirstName)
+
+		assert (testUser.getLastName() == testLastName)
+
+		assert (testUser.getMobileNumber() == testMobileNumber)
 		
 		if(testUser.getUsername() == testUsername) { // injected defect: "==" was intentionally changed to "!="
 			System.out.println(TestUtils.TEXT_COLOR_GREEN + "TC1-getUsername-Passed" + TestUtils.TEXT_COLOR_RESET);
