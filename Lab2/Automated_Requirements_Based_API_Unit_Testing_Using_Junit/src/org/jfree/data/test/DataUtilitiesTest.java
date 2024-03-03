d91a9472b962cb901c40643d230a42dc2ea5c219
40291291 <kwilliamson15@qub.ac.uk>
2024-03-03 18:00:26 +0000

    Refactored tests

--- Lab2/Automated_Requirements_Based_API_Unit_Testing_Using_Junit/src/org/jfree/data/test/DataUtilitiesTest.java
+++ Lab2/Automated_Requirements_Based_API_Unit_Testing_Using_Junit/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -58,7 +58,7 @@ public class DataUtilitiesTest {
 		dataArray = null;
 		dataValues = null;
 	}
-	
+
 	@Test
 	public void testCalculateColumnTotalWithValidDataAndColumnValue() { 
 		int column = 1;
@@ -66,23 +66,23 @@ public class DataUtilitiesTest {
 		assertEquals("When valid 2D matrix and column value supplied, 4.0 should be returned", 
 				4.0, DataUtilities.calculateColumnTotal(data, column), 0.0000001d); 
 	}
-	
-	@Test
+
+	@Test (expected = IndexOutOfBoundsException.class)
 	public void testCalculateColumnTotalWithValidDataAndInvalidColumnValueLessThan0() { 
 		int column = -1;
 		
 		assertEquals("When valid 2D matrix and invalid column value supplied, 0.0 should be returned", 
 				0.0, DataUtilities.calculateColumnTotal(data, column), 0.0000001d); 
 	}
-	
-	@Test
+
+	@Test (expected = IndexOutOfBoundsException.class)
 	public void testCalculateColumnTotalWithValidDataAndInvalidColumnValueGreaterThanNumberOfColumns() { 
 		int column = 13;
 		
 		assertEquals("When valid 2D matrix and invalid column value supplied, 0.0 should be returned", 
 				0.0, DataUtilities.calculateColumnTotal(data, column), 0.0000001d); 
 	}
-	
+
 	@Test
 	public void testCalculateColumnTotalWithEmptyDataAndValidColumnValue() { 
 		int column = 1;
@@ -94,7 +94,7 @@ public class DataUtilitiesTest {
 		assertEquals("When empty 2D matrix and valid column value supplied, 0.0 should be returned", 
 				0.0, DataUtilities.calculateColumnTotal(data, column), 0.0000001d); 
 	}
-	
+
 	@Test
 	public void testCalculateColumnTotalWithEmptyDataAndInvalidColumnValueLessThan0() { 
 		int column = -1;
@@ -106,7 +106,7 @@ public class DataUtilitiesTest {
 		assertEquals("When empty 2D matrix and invalid column value supplied, 0.0 should be returned", 
 				0.0, DataUtilities.calculateColumnTotal(data, column), 0.0000001d); 
 	}
-	
+
 	@Test
 	public void testCalculateColumnTotalWithEmptyDataAndInvalidColumnValueGreaterThanNumberOfColumns() { 
 		int column = 13;
@@ -118,7 +118,7 @@ public class DataUtilitiesTest {
 		assertEquals("When empty 2D matrix and invalid column value supplied, 0.0 should be returned", 
 				0.0, DataUtilities.calculateColumnTotal(data, column), 0.0000001d); 
 	}
-	
+
 	@Test
 	public void testCalculateColumnTotalWithNullDataAndValidColumnValue() { 
 		int column = 1;
@@ -132,7 +132,7 @@ public class DataUtilitiesTest {
 					e.getClass().equals(IllegalArgumentException.class));
 		}
 	}
-	
+
 	@Test
 	public void testCalculateColumnTotalWithNullDataAndInvalidColumnValueLessThan0() { 
 		int column = -1;
@@ -146,7 +146,7 @@ public class DataUtilitiesTest {
 					e.getClass().equals(IllegalArgumentException.class));
 		}
 	}
-	
+
 	@Test
 	public void testCalculateColumnTotalWithNullDataAndInvalidColumnValueGreaterThanNumberOfColumns() { 
 		int column = 13;
@@ -160,7 +160,7 @@ public class DataUtilitiesTest {
 					e.getClass().equals(IllegalArgumentException.class));
 		}
 	}
-	
+
 	@Test
 	public void testCalculateRowTotalWithValidDataAndRowValue() { 
 		int row = 0;
@@ -168,23 +168,23 @@ public class DataUtilitiesTest {
 		assertEquals("When valid 2D matrix and row value supplied, 12.0 should be returned", 
 				12.0, DataUtilities.calculateRowTotal(data, row), 0.0000001d); 
 	}
-	
-	@Test
+
+	@Test (expected = IndexOutOfBoundsException.class)
 	public void testCalculateRowTotalWithValidDataAndInvalidRowValueLessThan0() { 
 		int row = -1;
 		
 		assertEquals("When valid 2D matrix and invalid row value supplied, 0.0 should be returned", 
 				0.0, DataUtilities.calculateRowTotal(data, row), 0.0000001d); 
 	}
-	
-	@Test
+
+	@Test (expected = IndexOutOfBoundsException.class)
 	public void testCalculateRowTotalWithValidDataAndInvalidRowValueGreaterThanNumberOfRows() { 
 		int row = 13;
 		
 		assertEquals("When valid 2D matrix and invalid row value supplied, 0.0 should be returned", 
 				0.0, DataUtilities.calculateRowTotal(data, row), 0.0000001d); 
 	}
-	
+
 	@Test
 	public void testCalculateRowTotalWithEmptyDataAndValidRowValue() { 
 		int row = 0;
@@ -196,7 +196,7 @@ public class DataUtilitiesTest {
 		assertEquals("When empty 2D matrix and valid row value supplied, 0.0 should be returned", 
 				0.0, DataUtilities.calculateRowTotal(data, row), 0.0000001d); 
 	}
-	
+
 	@Test
 	public void testCalculateRowTotalWithEmptyDataAndInvalidRowValueLessThan0() { 
 		int row = -1;
@@ -208,7 +208,7 @@ public class DataUtilitiesTest {
 		assertEquals("When empty 2D matrix and invalid row value supplied, 0.0 should be returned", 
 				0.0, DataUtilities.calculateRowTotal(data, row), 0.0000001d); 
 	}
-	
+
 	@Test
 	public void testCalculateRowTotalWithEmptyDataAndInvalidRowValueGreaterThanNumberOfRows() { 
 		int row = 13;
@@ -220,7 +220,7 @@ public class DataUtilitiesTest {
 		assertEquals("When empty 2D matrix and invalid row value supplied, 0.0 should be returned", 
 				0.0, DataUtilities.calculateRowTotal(data, row), 0.0000001d); 
 	}
-	
+
 	@Test
 	public void testCalculateRowTotalWithNullDataAndValidRowValue() { 
 		int row = 0;
@@ -234,7 +234,7 @@ public class DataUtilitiesTest {
 					e.getClass().equals(IllegalArgumentException.class));
 		}
 	}
-	
+
 	@Test
 	public void testCalculateRowTotalWithNullDataAndInvalidRowValueLessThan0() { 
 		int row = -1;
@@ -248,7 +248,7 @@ public class DataUtilitiesTest {
 					e.getClass().equals(IllegalArgumentException.class));
 		}
 	}
-	
+
 	@Test
 	public void testCalculateRowTotalWithNullDataAndInvalidRowValueGreaterThanNumberOfRows() { 
 		int row = 13;
@@ -262,7 +262,7 @@ public class DataUtilitiesTest {
 					e.getClass().equals(IllegalArgumentException.class));
 		}
 	}
-	
+
 	@Test
 	public void testCreateNumberArrayWithValidDataArray() { 
 		Number[] expectedOutput = {1.5, 2.5, 3.5};
@@ -270,7 +270,7 @@ public class DataUtilitiesTest {
 		assertArrayEquals("When valid double array supplied, valid array of Number objects should be returned", 
 				expectedOutput, DataUtilities.createNumberArray(dataArray)); 
 	}
-	
+
 	@Test
 	public void testCreateNumberArrayWithEmptyDataArray() { 
 		Number[] expectedOutput = {};
@@ -280,7 +280,7 @@ public class DataUtilitiesTest {
 		assertArrayEquals("When empty double array supplied, an empty array of Number objects should be returned", 
 				expectedOutput, DataUtilities.createNumberArray(dataArray)); 
 	}
-	
+
 	@Test
 	public void testCreateNumberArrayWithNullDataArray() { 
 		try {
@@ -292,60 +292,47 @@ public class DataUtilitiesTest {
 					e.getClass().equals(IllegalArgumentException.class));
 		}
 	}
-	
+
 	@Test
 	public void testCreateNumberArrayWithValidDataArrayOfNegativeValues() { 
 		Number[] expectedOutput = {-1.5, -2.5, -3.5};
 		
-		dataArray = new double[3];
-		dataArray[0] = -1.5;
-		dataArray[1] = -2.5;
-		dataArray[2] = -3.5;
+		double[] dataArray = {-1.5, -2.5, -3.5};
 		
 		assertArrayEquals("When valid double array supplied, valid array of Number objects should be returned", 
 				expectedOutput, DataUtilities.createNumberArray(dataArray)); 
 	}
-	
+
 	@Test
 	public void testCreateNumberArrayWithValidDataArrayOfZeroValues() { 
 		Number[] expectedOutput = {0.0, 0.0, 0.0};
 		
-		dataArray = new double[3];
-		dataArray[0] = 0.0;
-		dataArray[1] = 0.0;
-		dataArray[2] = 0.0;
+		double[] dataArray = {0.0, 0.0, 0.0};
 		
 		assertArrayEquals("When valid double array supplied, valid array of Number objects should be returned", 
 				expectedOutput, DataUtilities.createNumberArray(dataArray)); 
 	}
-	
+
 	@Test
 	public void testCreateNumberArrayWithValidDataArrayOfLargeValues() { 
 		Number[] expectedOutput = {1000.0, 2000.0, 3000.0};
 		
-		dataArray = new double[3];
-		dataArray[0] = 1000.0;
-		dataArray[1] = 2000.0;
-		dataArray[2] = 3000.0;
+		double[] dataArray = {1000.0, 2000.0, 3000.0};
 		
 		assertArrayEquals("When valid double array supplied, valid array of Number objects should be returned", 
 				expectedOutput, DataUtilities.createNumberArray(dataArray)); 
 	}
-	
+
 	@Test
 	public void testCreateNumberArrayWithValidDataArrayOfMixedValues() { 
 		Number[] expectedOutput = {1.5, -2.5, 0.0, 3000.0};
 		
-		dataArray = new double[4];
-		dataArray[0] = 1.5;
-		dataArray[1] = -2.5;
-		dataArray[2] = 0.0;
-		dataArray[3] = 3000.0;
+		double[] dataArray = {1.5, -2.5, 0.0, 3000.0};
 		
 		assertArrayEquals("When valid double array supplied, valid array of Number objects should be returned", 
 				expectedOutput, DataUtilities.createNumberArray(dataArray)); 
 	} 
-	
+
 	@Test
 	public void testCreateNumberArray2DWithValidDataArray2D() { 
 		Number[][] expectedOutput = {{1.5, 2.5, 3.5}, {4.5, 5.5, 6.5}};
@@ -353,7 +340,7 @@ public class DataUtilitiesTest {
 		assertArrayEquals("When valid 2D double array supplied, valid 2D array of Number objects should be returned", 
 				expectedOutput, DataUtilities.createNumberArray2D(dataArray2D)); 
 	}
-	
+
 	@Test
 	public void testCreateNumberArray2DWithEmptyDataArray2D() { 
 		Number[][] expectedOutput = {};
@@ -363,7 +350,7 @@ public class DataUtilitiesTest {
 		assertArrayEquals("When empty 2D double array supplied, an empty 2D array of Number objects should be returned", 
 				expectedOutput, DataUtilities.createNumberArray2D(dataArray2D)); 
 	}
-	
+
 	@Test
 	public void testCreateNumberArray2DWithNullDataArray2D() { 
 		try {
@@ -375,77 +362,53 @@ public class DataUtilitiesTest {
 					e.getClass().equals(IllegalArgumentException.class));
 		}
 	}
-	
+
 	@Test
 	public void testCreateNumberArray2DWithValidDataArray2DOfNegativeValues() { 
 		Number[][] expectedOutput = {{-1.5, -2.5, -3.5}, {-4.5, -5.5, -6.5}};
 		
-		dataArray2D = new double[2][3];
-		dataArray2D[0][0] = -1.5;
-		dataArray2D[0][1] = -2.5;
-		dataArray2D[0][2] = -3.5;
-		dataArray2D[1][0] = -4.5;
-		dataArray2D[1][1] = -5.5;
-		dataArray2D[1][2] = -6.5;
+		double[][] dataArray2D = {{-1.5, -2.5, -3.5}, {-4.5, -5.5, -6.5}};
 		
 		assertArrayEquals("When valid 2D double array supplied, valid 2D array of Number objects should be returned", 
 				expectedOutput, DataUtilities.createNumberArray2D(dataArray2D)); 
 	}
-	
+
 	@Test
 	public void testCreateNumberArray2DWithValidDataArray2DOfZeroValues() { 
 		Number[][] expectedOutput = {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}};
 		
-		dataArray2D = new double[2][3];
-		dataArray2D[0][0] = 0.0;
-		dataArray2D[0][1] = 0.0;
-		dataArray2D[0][2] = 0.0;
-		dataArray2D[1][0] = 0.0;
-		dataArray2D[1][1] = 0.0;
-		dataArray2D[1][2] = 0.0;
+		double[][] dataArray2D = {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}};
 		
 		assertArrayEquals("When valid 2D double array supplied, valid 2D array of Number objects should be returned", 
 				expectedOutput, DataUtilities.createNumberArray2D(dataArray2D)); 
 	}
-	
+
 	@Test
 	public void testCreateNumberArray2DWithValidDataArray2DOfLargeValues() { 
 		Number[][] expectedOutput = {{1000.0, 2000.0, 3000.0}, {4000.0, 5000.0, 6000.0}};
 		
-		dataArray2D = new double[2][3];
-		dataArray2D[0][0] = 1000.0;
-		dataArray2D[0][1] = 2000.0;
-		dataArray2D[0][2] = 3000.0;
-		dataArray2D[1][0] = 4000.0;
-		dataArray2D[1][1] = 5000.0;
-		dataArray2D[1][2] = 6000.0;
+		double[][] dataArray2D = {{1000.0, 2000.0, 3000.0}, {4000.0, 5000.0, 6000.0}};
 		
 		assertArrayEquals("When valid 2D double array supplied, valid 2D array of Number objects should be returned", 
 				expectedOutput, DataUtilities.createNumberArray2D(dataArray2D)); 
 	}
-	
+
 	@Test
 	public void testCreateNumberArray2DWithValidDataArray2DOfMixedValues() { 
 		Number[][] expectedOutput = {{1.5, -2.5, 3.5}, {0.0, 4000.0, 0.0}};
 		
-		dataArray2D = new double[2][3];
-		dataArray2D[0][0] = 1.5;
-		dataArray2D[0][1] = -2.5;
-		dataArray2D[0][2] = 3.5;
-		dataArray2D[1][0] = 0.0;
-		dataArray2D[1][1] = 4000.0;
-		dataArray2D[1][2] = 0.0;
+		double[][] dataArray2D = {{1.5, -2.5, 3.5}, {0.0, 4000.0, 0.0}};
 		
 		assertArrayEquals("When valid 2D double array supplied, valid 2D array of Number objects should be returned", 
 				expectedOutput, DataUtilities.createNumberArray2D(dataArray2D)); 
 	}
-	
+
 	@Test
 	public void testGetCumulativePercentagesWithValidData() { 
 		assertEquals("When valid data is supplied, the cumulative percentage should be 1.0", 
 				1.0, (double) dataValues.getValue(2), 0.000000001d);
 	}
-	
+
 	@Test
 	public void testGetCumulativePercentagesWithEmptyData() { 
 		DefaultKeyedValues testKeyValues = new DefaultKeyedValues();
@@ -461,7 +424,7 @@ public class DataUtilitiesTest {
 					e.getClass().equals(IllegalArgumentException.class));
 		}
 	}
-	
+
 	@Test
 	public void testGetCumulativePercentagesWithNullData() { 
 		try {
